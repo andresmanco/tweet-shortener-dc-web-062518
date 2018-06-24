383c4323da7490cd63e4984a97150d20d978aa6c
Andres Manco <andresmanco@gmail.com>
2018-06-24 19:51:50 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 def dictionary
   dict =   {
+<<<<<<< HEAD
     "hello" => 'hi',
     "to" => '2',
     "two" => '2',
@@ -12,18 +13,35 @@ def dictionary
     'you' => 'u',
     "at" => "@", 
     "and" => "&"
+=======
+    "hello " => 'hi ',
+    "to " => '2 ',
+    "two " => '2 ',
+    "too " => '2 ', 
+    "for " => '4 ',
+    "four " => '4 ',
+    'be ' => 'b ',
+    'you ' => 'u ',
+    "at " => "@ ", 
+    "and " => "& "
+>>>>>>> 81008edac1e54e82baeeaa6163c77a6950204d67
   }
 end
 
 
 
 def word_substituter(tweet_one)
+<<<<<<< HEAD
   dictionary.keys.collect do |d|
     tweet_one.gsub! d  do |s|
       if s.length == d.length
         dictionary[d]
       end
     end
+=======
+    dictionary.keys.collect do |d|
+      tweet_one.gsub! d, dictionary[d]
+>>>>>>> 81008edac1e54e82baeeaa6163c77a6950204d67
   end 
   tweet_one
 end
